cb01836e5c600bc61dac91dab2afedf79b874f4d
faridk <git@faridk.com>
2019-07-25 20:17:55 -0700

    Make Alien bounce off the sides

--- main.py
+++ main.py
@@ -10,52 +10,51 @@ class Game:
 	timer = time.time()
 	def __init__ (self):
 		self.font = pygame.font.SysFont('Comic Sans MS', 30)
-		self.WIDTH, self.HEIGHT = pygame.display.get_surface().get_size()
+		Game.WIDTH, Game.HEIGHT = pygame.display.get_surface().get_size()
 		self.background_color = (0, 0, 0)
 		self.score = 0
 		self.player = Player("player", Game.SPRITE_FOLDER + "BattleBus.png", self.WIDTH - 160, self.HEIGHT - 140, 160, 140)
 		self.start()
-	
+
 	def start(self):
 		while True:
 			self.draw()
 			self.player.draw()
-			
+	
 			# Draw all bullets in the air
 			for bullet in Player.bullets:
 				bullet.draw()
 
 			# Draw all enemies of the Player
 			for enemy in Player.enemies:
-				enemy.x += 1
 				enemy.draw()
-			
-			
+
 	def quit(self):
-			pygame.quit() # Close the window
-			sys.exit()
-	
+		pygame.quit() # Close window
+		sys.exit()
+
 	def draw(self):
 		for event in pygame.event.get():
-				# Handle exit
-				if event.type == pygame.QUIT:
-						self.quit()
-		
+			# Handle exit
+			if event.type == pygame.QUIT:
+				self.quit()
+	
 		# Handle bar movement using keysself.player = Player(self, "player", Game.SPRITE_FOLDER + "BattleBus.png", self.WIDTH - 160, self.HEIGHT - 140, 160, 140)
 		pressed = pygame.key.get_pressed()
+		if pressed[pygame.K_q] or pressed[pygame.K_ESCAPE]:
+			self.quit()
 		if pressed[pygame.K_RIGHT]:
-				self.player.x += self.player.speed
-				self.player.direction_left = False
+			self.player.x += self.player.speed
+			self.player.direction_left = False
 		if pressed[pygame.K_LEFT]:
-				self.player.x -= self.player.speed
-				self.player.direction_left = True
+			self.player.x -= self.player.speed
+			self.player.direction_left = True
 		if pressed[pygame.K_SPACE]:
-				self.player.shoot()
+			self.player.shoot()
 
 		pygame.display.flip() # Updates the display
 		self.surface.fill(self.background_color) # Clear the screen, leave no smudges
-
-		
+	
 class Sprite:
 	def __init__(self, name, path, x, y, w, h):
 		self.name = name
@@ -70,21 +69,21 @@ class Sprite:
 	def draw(self):
 		Game.surface.blit(self.sprite, (self.x, self.y))
 
-		
+	
 class Player(Sprite):
 	bullets = []
 	enemies = []
 	def __init__(self, name, path, x, y, w, h):
 		super(Player, self).__init__(name, path, x, y, w, h)
 		self.direction_left = False
-		self.flipped_sprite = pygame.transform.flip(self.sprite, True, False)
+		self.flipped_sprite = pygame.transform.flip(self.sprite, True, False) 
 		self.bullets_per_second = 5
 		self.speed = 5
 		self.add_enemies()
 
 	def draw(self):
 		if self.direction_left:
-			  # Draw flipped
+			# Draw flipped
 			Game.surface.blit(self.flipped_sprite, (self.x, self.y))
 		else:
 			# Draw normal
@@ -96,24 +95,63 @@ class Player(Sprite):
 		if time.time() - Game.timer > 1 / self.bullets_per_second:
 			Player.bullets.append(bullet)
 			Game.timer = time.time()
-	
-	def add_enemies(self):
-	  alien = Alien("alien", Game.SPRITE_FOLDER + "Spaceship.png", 100, 25, 100, 100)
-	  gargoyle = Gargoyle("gargoyle", Game.SPRITE_FOLDER + "Gargoyle.png", 100, 100, 100, 100)
-	  Player.enemies.append(alien)
 		
+	def add_enemies(self):
+		alien = Alien("alien", Game.SPRITE_FOLDER + "Spaceship.png", 100, 25, 100, 100)
+		gargoyle = Gargoyle("gargoyle", Game.SPRITE_FOLDER + "Gargoyle.png", 100, 100, 100, 100)
+		Player.enemies.append(alien)
+	
 
-class Alien(Sprite):
+class Enemy(Sprite):
+	def __init__(self, name, path, x, y, w, h):
+		super(Enemy, self).__init__(name, path, x, y, w, h)
+		self.right = True
+		self.left = False
+		self.up = False
+		self.down = False
+		self.last_y = 0
+	
+	def draw(self):
+		super(Enemy, self).draw()
+		# Move in a defined direction
+		if self.right:
+			self.x += self.speed
+		elif self.left:
+			self.x -= self.speed
+		if self.up:
+			self.y -= self.speed
+		elif self.down:
+			self.y += self.speed
+		# Bounce by setting directions
+		if self.x > Game.WIDTH - self.w:
+			self.right = False
+			# Move down
+			self.down = True
+		if self.x < 0:
+			self.left = False
+			# Move down
+			self.down = True
+		# Move down only 100 pixels
+		if self.y - self.last_y > 100:
+			self.down = False
+			self.last_y = self.y
+			if self.x < 0:
+				self.right = True
+			if self.x > Game.WIDTH - self.w:
+				self.left = True
+			
+
+
+class Alien(Enemy):
 	def __init__(self, name, path, x, y, w, h):
 		super(Alien, self).__init__(name, path, x, y, w, h)
-		self.speed = 2
+		self.speed = 1
 
 	
-class Gargoyle(Sprite):
+class Gargoyle(Enemy):
 	def __init__(self, name, path, x, y, w, h):
 		super(Gargoyle, self).__init__(name, path, x, y, w, h)
-		self.speed = 3.5
-	
+		self.speed = 200
 class Bullet(Sprite):
 	def __init__(self, name, path, x, y, w, h):
 		super(Bullet, self).__init__(name, path, x, y, w, h)
@@ -125,6 +163,5 @@ class Bullet(Sprite):
 
 	def hit(self):
 		pass
-		
-
-game = Game() 
+	
+game = Game()
